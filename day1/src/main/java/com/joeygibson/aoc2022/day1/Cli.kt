c9597ab52d32a78f185c5708214561b4bd32083c
Joey Gibson <joey@joeygibson.com>
2022-12-01 12:17:31 -0500

    day 1, part 1

---
+++ day1/src/main/java/com/joeygibson/aoc2022/day1/Cli.kt
@@ -0,0 +1,52 @@
+package com.joeygibson.aoc2022.day1
+
+import org.apache.logging.log4j.LogManager
+import picocli.CommandLine
+import java.io.BufferedReader
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.Callable
+
+@CommandLine.Command(
+    name = "day1",
+    mixinStandardHelpOptions = true,
+    version = ["day1 1.0.0"],
+    description = ["AoC 2022 day1"]
+)
+class Cli : Callable<Int> {
+    @CommandLine.Parameters(index = "0..*", description = ["The file(s) to process"])
+    var files: Array<File>? = null
+
+    @Throws(IOException::class)
+    override fun call(): Int {
+        files?.forEach { file ->
+            val fileName = file.canonicalPath
+
+            val elvesCalories = mutableListOf<Int>()
+            val calories = mutableListOf<Int>()
+
+            BufferedReader(file.reader()).useLines { lines ->
+                lines.forEach { line ->
+                    if (line.isBlank()) {
+                        if (!calories.isEmpty()) {
+                            elvesCalories.add(calories.sum())
+                            calories.clear()
+                        }
+                    } else {
+                        calories.add(line.toInt())
+                    }
+                }
+            }
+
+            println(elvesCalories.max())
+        }
+
+        return 0
+    }
+
+    companion object {
+        private val logger = LogManager.getLogger(
+            Cli::class.java
+        )
+    }
+}
