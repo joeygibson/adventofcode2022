76220bfd75cff7ca4e64909b0f0466553d7a0983
Joey Gibson <joey@joeygibson.com>
2022-12-03 00:55:20 -0500

    added readWithoutBlanks to template

--- template/src/main/java/com/joeygibson/aoc2022/XXX/Util.kt
+++ template/src/main/java/com/joeygibson/aoc2022/XXX/Util.kt
@@ -3,4 +3,6 @@ package com.joeygibson.aoc2022.XXX
 import java.io.File
 
 fun readInput(file: File) = file.readLines()
+
+fun readWithoutBlanks(file: File) = readInput(file).filter { it.isNotBlank() }
 	
