efd5966fbf08b6e4dc6c32f625b0e00a1e42a941
Joey Gibson <joey@joeygibson.com>
2022-12-10 01:13:00 -0500

    day 10, part 1

---
+++ day10/src/main/java/com/joeygibson/aoc2022/day10/App.kt
@@ -0,0 +1,81 @@
+package com.joeygibson.aoc2022.day10
+
+import picocli.CommandLine
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.Callable
+import kotlin.system.exitProcess
+
+@CommandLine.Command(
+    name = "day10",
+    mixinStandardHelpOptions = true,
+    version = ["day10 1.0.0"],
+    description = ["AoC 2022 day10"]
+)
+class App : Callable<Int> {
+    @CommandLine.Parameters(index = "0", description = ["The file to process"])
+    val file: File? = null
+
+    @Throws(IOException::class)
+    override fun call(): Int {
+        if (file == null) {
+            println("Usage: day10 <input file>")
+            exitProcess(1)
+        }
+
+        val lines = readWithoutBlanks(file)
+
+        printResults("part1", part1(lines))
+        printResults("part2", part2(lines))
+
+        return 0
+    }
+
+    private fun part1(lines: List<String>): Int {
+        val cycleValues = mutableMapOf<Int, Int>()
+        val cyclesToCheck = listOf(20, 60, 100, 140, 180, 220)
+        var register = 1
+        var cycle = 0
+
+        lines.forEach { line ->
+            cycle++
+
+            if (cyclesToCheck.contains(cycle)) {
+                cycleValues[cycle] = register
+            }
+
+            when {
+                line.startsWith("addx") -> {
+                    val value = line.split(" ")[1].toInt()
+
+                    repeat(2) { c ->
+                        if (cyclesToCheck.contains(cycle)) {
+                            cycleValues[cycle] = register
+                        }
+
+                        if (c == 1) {
+                            register += value
+                        } else {
+                            cycle++
+                        }
+                    }
+                }
+            }
+        }
+
+        cycle++
+
+        println("cycle: $cycle, register: $register")
+        println(cycleValues)
+
+        return cycleValues.map { (key, value) ->
+            key * value
+        }.sum()
+    }
+
+    private fun part2(lines: List<String>): Any {
+        // part 2 goes here
+
+        return "no result for part 2"
+    }
+}
---
+++ day10/src/main/java/com/joeygibson/aoc2022/day10/Util.kt
@@ -0,0 +1,16 @@
+package com.joeygibson.aoc2022.day10
+
+import java.io.File
+
+fun readInput(file: File) = file.readLines()
+
+fun readWithoutBlanks(file: File) = readInput(file).filter { it.isNotBlank() }
+
+fun printResults(part: String, result: Any) = println("$part: $result")
+
+fun <T> List<T>.splitOn(predicate: (T) -> Boolean): List<List<T>> {
+    val top = this.takeWhile { predicate(it) }
+    val rest = this.dropWhile { !predicate(it) }.drop(1)
+
+    return listOf(top, rest)
+}
