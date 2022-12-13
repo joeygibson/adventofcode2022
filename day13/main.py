92feb0c9db32f293884062ef4f5a14e3ba1e87f2
Joey Gibson <joey@joeygibson.com>
2022-12-13 15:21:33 -0500

    day 13, part 1 - wip2

--- day13/main.py
+++ day13/main.py
@@ -7,47 +7,52 @@ def part1(lines: list[str]) -> int:
 
     groups = [lines[i * 2:(i + 1) * 2] for i in range((len(lines) + 2 - 1) // 2)]
 
-    group_results = {}
+    group_results = []
     for (group_id, group) in enumerate(groups):
-        # for pairs in zip(group[0], group[1]):
-        #     print(f'pairs: {pairs}')
-
         res = ordered_lists(group[0], group[1])
 
-        print(f'res: {res}')
-        group_results[group_id] = True
+        print(f'{group}: {res}')
+        group_results.append(res)
+
+    print(f'results: {group_results}')
+
+    result = 0
+    for index, value in enumerate(group_results):
+        if value:
+            result += index + 1
+
+    return result
 
 
-def ordered_lists(a: list, b: list) -> bool:
+def ordered_lists(a: list, b: list, recurse: bool = True) -> bool:
     ordered = False
 
     if a == b:
         ordered = True
     else:
         for i in range(0, max(len(a), len(b))):
-            if i > len(a):
-                ordered = True
-                break
-            elif i > len(b) is None:
+            if len(a) and not len(b):
                 ordered = False
+                break
+            elif len(b) and not len(a):
+                ordered = True
                 break
             else:
                 if isinstance(a[0], int) and isinstance(b[0], int):
-                    if not ordered_ints(a[0], a[1]):
-                        ordered = False
-                        break
+                    if ordered_ints(a[0], b[0]):
+                        ordered = True
                 elif isinstance(a[0], list) and isinstance(b[0], int):
-                    if not ordered_lists(a[0], [b[0]]):
-                        ordered = False
-                        break
+                    if ordered_lists(a[0], [b[0]], False):
+                        ordered = True
                 elif isinstance(a[0], int) and isinstance(b[0], list):
-                    if not ordered_lists([a[0]], b[0]):
-                        ordered = False
-                        break
+                    if ordered_lists([a[0]], b[0], False):
+                        ordered = True
                 else:
-                    if not ordered_lists(a[0], b[0]):
-                        ordered = False
-                        break
+                    if ordered_lists(a[0], b[0]):
+                        ordered = True
+
+                if ordered and recurse:
+                    ordered = ordered_lists(a[1:], b[1:])
 
     return ordered
 
