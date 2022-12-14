bcd71a097a52dd02e5a0229dbb30d6d95cc1fc0f
Joey Gibson <joey@joeygibson.com>
2022-12-14 14:43:12 -0500

    day 14, part 1

--- day14/src/main/java/com/joeygibson/aoc2022/day14/App.kt
+++ day14/src/main/java/com/joeygibson/aoc2022/day14/App.kt
@@ -7,6 +7,10 @@ import java.io.IOException
 import java.util.concurrent.Callable
 import kotlin.system.exitProcess
 
+private const val ROCK = "#"
+private const val AIR = "."
+private const val SAND = "o"
+
 @CommandLine.Command(
     name = "day14",
     mixinStandardHelpOptions = true,
@@ -18,6 +22,8 @@ class App : Callable<Int> {
     val file: File? = null
 
     private lateinit var terminal: Terminal
+
+    private val theMap = mutableMapOf<Int, MutableMap<Int, String>>().withDefault { mutableMapOf() }
 
     @Throws(IOException::class)
     override fun call(): Int {
@@ -38,13 +44,10 @@ class App : Callable<Int> {
     }
 
     private fun part1(lines: List<String>): Any {
-        val theMap = mutableMapOf<Int, MutableMap<Int, String>>().withDefault { mutableMapOf() }
-
         lines.forEach { line ->
             line.split(" -> ")
                 .map { it.split(",").map(String::toInt) }
                 .windowed(2) { (first, second) ->
-//                    println("first: $first, second: $second")
                     var startRow = first[1]
                     var endRow = second[1]
                     var startCol = first[0]
@@ -62,7 +65,7 @@ class App : Callable<Int> {
 
                         (startRow..endRow).forEach { row ->
                             val rowMap = theMap.getOrPut(row) { mutableMapOf() }
-                            rowMap[startCol] = "#"
+                            rowMap[startCol] = ROCK
                         }
                     }
 
@@ -75,17 +78,85 @@ class App : Callable<Int> {
 
                         (startCol..endCol).forEach { col ->
                             val colMap = theMap.getOrPut(startRow) { mutableMapOf() }
-                            colMap[col] = "#"
+                            colMap[col] = ROCK
                         }
                     }
                 }
         }
 
-        drawMap(theMap)
-        return "no result for part 1"
+        drawMap()
+
+        var grainsOfSand = 0
+        var fallingIntoTheVoid = false
+
+        while (!fallingIntoTheVoid) {
+            var col = 500
+            var row = 1
+            var cameToRest = false
+
+            outerLoop@ while (!cameToRest) {
+                if (isOpen(row, col)) {
+                    row++
+                    continue
+                }
+
+                // decide left or right
+                val left = col - 1
+                val right = col + 1
+
+                if (isOpen(row, left)) {
+                    if (isTheVoid(left)) {
+                        fallingIntoTheVoid = true
+                        break
+                    }
+
+                    col = left
+                    continue
+                } else if (isOpen(row, right)) {
+                    if (isTheVoid(right)) {
+                        fallingIntoTheVoid = true
+                        break
+                    }
+
+                    col = right
+                    continue
+                } else {
+                    val colMap = theMap.getOrPut(row - 1) { mutableMapOf() }
+                    colMap[col] = SAND
+                    cameToRest = true
+                    grainsOfSand++
+                    break@outerLoop
+                }
+            }
+
+            drawMap()
+        }
+
+        drawMap()
+
+        return grainsOfSand
     }
 
-    private fun drawMap(theMap: MutableMap<Int, MutableMap<Int, String>>) {
+    private fun isTheVoid(col: Int): Boolean {
+        val maxCols = theMap.values
+            .flatMap { it.keys }
+            .max()
+        val minCols = theMap.values
+            .flatMap { it.keys }
+            .min()
+
+        return col < minCols || col > maxCols
+    }
+
+    private fun isOpen(
+        row: Int, col: Int
+    ): Boolean {
+        val space = theMap[row]?.get(col)
+
+        return space == null
+    }
+
+    private fun drawMap() {
         val maxRows = theMap.keys.max()
         val minRows = theMap.keys.min()
         val maxCols = theMap.values
@@ -97,8 +168,8 @@ class App : Callable<Int> {
 
 //        println("minCols: $minCols, maxCols: $maxCols, minRows: $minRows, maxRows: $maxRows")
 
-        (0 .. maxRows).forEach { row ->
-            (minCols .. maxCols).forEach { col ->
+        (0..maxRows).forEach { row ->
+            (minCols..maxCols).forEach { col ->
                 val value = theMap[row]?.get(col) ?: "."
 
                 if (row == 0 && col == 500) {
